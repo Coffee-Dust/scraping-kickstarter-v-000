0e8d1c87576e93998b4bcd87ac4a833972f79797
Jonathan Dust <d24dust@hotmail.com>
2018-05-16 20:41:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
       image_link: project_card.css(".project-thumbnail a img").attribute("src"),
       description: project_card.css("p.bbcard_blurb").text,
       location: project_card.css(".project-meta span.location-name").text,
-      percent_funded: project_card.css(".project-stats .first.funded strong").text
+      percent_funded: project_card.css(".project-stats .first.funded strong").text.to_n
     }
   end
   projects
