11afcfb6198ba10d9f21d619650eafd109ba7044
Jonathan Dust <d24dust@hotmail.com>
2018-05-16 20:40:39 +0000

    automatically backed up by learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -44,3 +44,21 @@ describe "#create_project_hash" do
   end
 
 end
+
+
+
+
+# :projects => {
+#   "Title"  => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   },
+#   "Another Great Project" => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   }
+# }
