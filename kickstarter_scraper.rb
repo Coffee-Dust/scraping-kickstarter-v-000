974523343cf59cabd9b0fda194e3c8b10a370a25
Jonathan Dust <d24dust@hotmail.com>
2018-05-16 20:26:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,10 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
- 
+
   projects = {}
 
+  kickstarter.css("#projects_list .project.grid_4")
 
   projects
 end
